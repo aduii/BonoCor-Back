781d10cbdfa1de0286375c1fe2cd3a75dfaaae5e
Diana Navarro <navarroruiz.diana@gmail.com>
2020-06-30 07:07:49 -0500

    Include cors

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 
+var cors = require('cors');
+
 var app = express();
 
 // view engine setup
@@ -19,6 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+app.options('*', cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
